aeb7450a1327248a1d06cfd48704ef11d5d8f6cc
RaghuRam <raghu@v-comply.com>
2025-08-21 14:59:11 +0530

    Add start script for deployment

--- src/routes/formRoutes.ts
+++ src/routes/formRoutes.ts
@@ -3,6 +3,12 @@ import { submitForm } from "../controllers/formController";
 
 const router = Router();
 
+// Test route (GET)
+router.get("/", (req, res) => {
+  res.send("Form API is running 🚀");
+});
+
+// Submit form (POST)
 router.post("/", submitForm);
 
 export default router;
