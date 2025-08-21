8ec1725c7398f70c796eb7fbd784151d64e11303
RaghuRam <raghu@v-comply.com>
2025-08-21 10:31:04 +0530

    Add start script for deployment

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,16 @@
 import express from "express";
 import * as dotenv from "dotenv";
 import cors from "cors";
-import connectDB from "./config/db";
-import formRoutes from "./routes/formRoutes";
+import connectDB from "./config/db.ts";
+import formRoutes from "./routes/formRoutes.ts";
 
 dotenv.config();
 const app = express();
 
-// Middleware
 app.use(cors());
 app.use(express.json());
-
-// Routes
 app.use("/api/forms", formRoutes);
 
-// DB + Server
 const PORT = process.env.PORT || 4000;
 connectDB().then(() => {
   app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
